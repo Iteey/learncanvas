61efa5f081f2f6879b0f40409a60841c4229fd7c
Denis Lesnikov <102676229+Iteey@users.noreply.github.com>
2023-06-07 15:56:33 +0300

    task completed
    +arrow limit
    +item that gives arrows(but no texture, just black square)

--- src/MainScene.js
+++ src/MainScene.js
@@ -3,13 +3,18 @@ import atlas from './assets/0x72_DungeonTilesetII_v1.4.png';
 import atlasJSON from './assets/atlas.json';
 import mapJSON from './assets/map.json';
 import { Player } from "./Player";
+import { InputManager } from "./InputManager";
+
 export class MainScene extends Scene {
-      
+    constructor() {
+        super({ key: 'MainScene' });
+    }
+
     preload() {
         this.load.atlas('atlas', atlas, atlasJSON);
         this.load.tilemapTiledJSON('map', mapJSON);
     }
-    
+
     create() {
         const map = this.make.tilemap({ key: 'map' });
         const tiles = map.addTilesetImage('0x72_DungeonTilesetII_v1.4', 'atlas');
@@ -18,11 +23,57 @@ export class MainScene extends Scene {
         floor.setCollisionByExclusion([130]);
         const walls = map.createLayer(1, tiles, 0, 0);
         walls.setScale(4);
-        let player = this.add.existing(new Player(this, 100, 100));
         const edges = map.createLayer(2, tiles, 0, 0);
         edges.setScale(4);
-        this.physics.add.collider(player, floor);
         
+        this.player = new Player(this, 100, 100);
+        this.add.existing(this.player);
+        this.physics.add.collider(this.player, floor);
+
+        this.arrowCountText = this.add.text(10, 10, `Arrows: 0/${this.player.maxArrows}`, {
+            fontFamily: 'Arial',
+            fontSize: 16,
+            color: '#ffffff',
+        });
+
+        this.inputManager = new InputManager(this);
+
+        // Spawn item
+        this.spawnItem(400, 200);
+        this.load.image('arrows', './assets/arrows.png');
+
     }
-    
+
+    spawnItem(x, y) {
+        const item = this.physics.add.sprite(x, y, 'atlas', 'item');
+        this.physics.add.overlap(this.player, item, () => {
+            this.player.pickUpItem();
+            item.destroy();
+        });
+        item.setTexture('arrows');
+
+    }
+
+    update() {
+        const { keys } = this.inputManager;
+        
+        // Player movement
+        if (keys.KeyA.isDown) {
+            this.player.body.setVelocityX(-this.player.body.maxSpeed);
+            this.player.setFlipX(true);
+        } else if (keys.KeyD.isDown) {
+            this.player.body.setVelocityX(this.player.body.maxSpeed);
+            this.player.setFlipX(false);
+        } else {
+            this.player.body.setVelocityX(0);
+        }
+
+        if (keys.KeyW.isDown) {
+            this.player.body.setVelocityY(-this.player.body.maxSpeed);
+        } else if (keys.KeyS.isDown) {
+            this.player.body.setVelocityY(this.player.body.maxSpeed);
+        } else {
+            this.player.body.setVelocityY(0);
+        }
+    }
 }
--- src/Player.js
+++ src/Player.js
@@ -12,41 +12,33 @@ export class Player extends Physics.Arcade.Sprite {
         this.body.setOffset(0, 12);
         this.body.setSize(16, 16, false);
         this.body.setMaxSpeed(400);
-
         this.body.setDrag(800, 800);
-      
         this.anims.create({
             key: 'elf_m_idle_anim',
             frames: this.anims.generateFrameNames('atlas', { prefix: 'elf_m_idle_anim_', start:0, end: 3}),
             repeat: -1,
             frameRate: 8,
-     
         });
-        
         this.anims.create({
             key: 'elf_m_run_anim',
             frames: this.anims.generateFrameNames('atlas', { prefix: 'elf_m_run_anim_', start:0, end: 3}),
             repeat: -1,
             frameRate: 8,
-            
         });
         this.anims.create({
             key: 'elf_m_hit_anim',
             frames: this.anims.generateFrameNames('atlas', { prefix: 'elf_m_hit_anim_', start:0, end: 0}),
             repeat: -1,
             frameRate: 8,
-      
         });
-        
         this.anims.play('elf_m_idle_anim');
-    
         this.setScale(4);
         this.input = new InputManager(scene);
-       
+        this.arrowCount = 0;
+        this.maxArrows = 20;
     }
     isMoving(){
-   
-        return this.body.speed>0;
+        return this.body.speed > 0;
     }
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
@@ -60,24 +52,26 @@ export class Player extends Physics.Arcade.Sprite {
         }
         if(this.input.keys.KeyW.isDown) {
             this.body.setVelocityY(-this.body.maxSpeed);
-           
         }
         if(this.input.keys.KeyS.isDown) {
             this.body.setVelocityY(this.body.maxSpeed);
-            
         }
         if(this.input.keys.Space.isDown){
-            
-            if(time-this.lastShotTime > this.shootInterval){
-                this.scene.add.existing(new Bullet(this.scene, this.x,this.y, this.input.mouse));
+            if(time - this.lastShotTime > this.shootInterval && this.arrowCount < this.maxArrows && time - this.lastShotTime > this.shootInterval){
+                this.scene.add.existing(new Bullet(this.scene, this.x, this.y, this.input.mouse));
                 this.lastShotTime = time;
-            }         
+                this.arrowCount++;
+                this.scene.arrowCountText.setText(`Arrows: ${this.arrowCount}/${this.maxArrows}`);
+            }
         }
-
         if(this.isMoving()){
             this.play('elf_m_run_anim', true);
         } else {
             this.play('elf_m_idle_anim', true);
         }
+    }
+    pickUpItem() {
+        this.arrowCount = 0;
+        this.scene.arrowCountText.setText(`Arrows: ${this.arrowCount}/${this.maxArrows}`);
     }
 }
